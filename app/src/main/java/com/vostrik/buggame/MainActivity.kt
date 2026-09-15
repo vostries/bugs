ea7421172ec1e840837146542b2d69661165fb05
Nenorii <sasha.balabanov05@gmail.com>
2026-09-15 21:01:15 +0700

    lab2: part 1 - player data structure and basic UI

--- app/src/main/java/com/vostrik/buggame/MainActivity.kt
+++ app/src/main/java/com/vostrik/buggame/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.vostrik.buggame
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var player: Player
+    private lateinit var editTextFullName: EditText
+    private lateinit var radioGroupGender: RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        player = Player()
+
+        editTextFullName = findViewById(R.id.editTextFullName)
+        radioGroupGender = findViewById(R.id.radioGroupGender)
     }
 }
---
+++ app/src/main/java/com/vostrik/buggame/Player.kt
@@ -0,0 +1,10 @@
+package com.vostrik.buggame
+
+data class Player(
+    var fullName: String = "",
+    var gender: String = "",
+    var course: Int = 1,
+    var difficulty: Int = 1,
+    var birthDate: String = "",
+    var zodiacSign: String = ""
+)
