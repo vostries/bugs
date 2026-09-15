868b353033ddbf42098fe443950cf12cc7c4663e
vostries <hzz@ro.ru>
2026-09-15 21:15:34 +0700

    lab2: part 4 - add difficulty seekbar

--- app/src/main/java/com/vostrik/buggame/MainActivity.kt
+++ app/src/main/java/com/vostrik/buggame/MainActivity.kt
@@ -5,7 +5,9 @@ import android.widget.ArrayAdapter
 import android.widget.CalendarView
 import android.widget.EditText
 import android.widget.RadioGroup
+import android.widget.SeekBar
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.Calendar
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editTextFullName: EditText
     private lateinit var radioGroupGender: RadioGroup
     private lateinit var spinnerCourse: Spinner
+    private lateinit var seekBarDifficulty: SeekBar
+    private lateinit var textViewDifficulty: TextView
     private lateinit var calendarView: CalendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,12 +32,24 @@ class MainActivity : AppCompatActivity() {
         editTextFullName = findViewById(R.id.editTextFullName)
         radioGroupGender = findViewById(R.id.radioGroupGender)
         spinnerCourse = findViewById(R.id.spinnerCourse)
+        seekBarDifficulty = findViewById(R.id.seekBarDifficulty)
+        textViewDifficulty = findViewById(R.id.textViewDifficulty)
         calendarView = findViewById(R.id.calendarView)
 
         val courses = resources.getStringArray(R.array.courses)
         spinnerCourse.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, courses).apply {
             setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         }
+
+        seekBarDifficulty.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val difficulty = progress + 1
+                textViewDifficulty.text = difficulty.toString()
+                player.difficulty = difficulty
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
 
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             val calendar = Calendar.getInstance()
