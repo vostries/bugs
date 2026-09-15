cae8357f7a77b54e2a81f454757892892c3eb9f3
Nenorii <sasha.balabanov05@gmail.com>
2026-09-15 21:13:54 +0700

    lab2: part 3 - birth date calendar

--- app/src/main/java/com/vostrik/buggame/MainActivity.kt
+++ app/src/main/java/com/vostrik/buggame/MainActivity.kt
@@ -2,10 +2,14 @@ package com.vostrik.buggame
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.CalendarView
 import android.widget.EditText
 import android.widget.RadioGroup
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var editTextFullName: EditText
     private lateinit var radioGroupGender: RadioGroup
     private lateinit var spinnerCourse: Spinner
+    private lateinit var calendarView: CalendarView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +28,18 @@ class MainActivity : AppCompatActivity() {
         editTextFullName = findViewById(R.id.editTextFullName)
         radioGroupGender = findViewById(R.id.radioGroupGender)
         spinnerCourse = findViewById(R.id.spinnerCourse)
+        calendarView = findViewById(R.id.calendarView)
 
         val courses = resources.getStringArray(R.array.courses)
         spinnerCourse.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, courses).apply {
             setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val calendar = Calendar.getInstance()
+            calendar.set(year, month, dayOfMonth)
+            val dateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+            player.birthDate = dateFormat.format(calendar.time)
         }
     }
 }
